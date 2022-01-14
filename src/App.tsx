4e30e85c6a7f22f20bf87e7d2e0c6646069e9498
하상영 <sangyouh@usc.edu>
2022-01-14 14:23:48 +0900

    TypeScript Practice: mid commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
 import "./App.css";
 import Header from "./Header";
+import React, { useState } from "react";
 
 function App() {
+  const [number, setNumber] = useState<number | string>(5);
+
+  const [people, setPeople] = useState([
+    {
+      name: "Sean Mendez",
+      url: "",
+      age: 35,
+      note: "",
+    },
+    {
+      name: "Mike Jacobs",
+      url: "",
+      age: 30,
+    },
+  ]);
+
+  const changeNumber = () => {
+    setNumber(10);
+  };
+
+  const changeNumberToString = () => {
+    setNumber("9");
+  };
+
+  //TypeScript Complains
+  // const changeNumberToBoolean = () => {
+  //   setNumber(true);
+  // }
+
   return (
     <div className="App">
       <Header title="Hello World" color="red" />
+      <h1>TypeScript Practice!!!!!!</h1>
     </div>
   );
 }
